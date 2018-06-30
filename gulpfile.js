bdb6fc2c5dded26674c5afa4b717b602b68a5cc2
captainyarr <captainyarr42@gmail.com>
2018-06-30 12:38:05 -0700

    GulpArmSupport: Add support for linuxarm platform

--- gulpfile.js
+++ gulpfile.js
@@ -11,14 +11,26 @@ var gzip = require('gulp-gzip');
 var tar = require('gulp-tar');
 var package = require('./package.json');
 
-const nwVersion = '0.20.1';
+var nwVersion = '0.20.1';
+var buildDownloadUrl = 'https://get.popcorntime.sh/repo/nw/';
+var buildplatforms = argv.p ? argv.p.split(',') : [detectCurrentPlatform()];
+
+//Platform specific overrides if needed
+
+if (buildplatforms.indexOf("linuxarm") >=0 ) {
+    nwVersion = '0.31.2';
+}
+
+if (buildplatforms.indexOf("linuxarm") >= 0) {
+    buildDownloadUrl = 'https://dl.nwjs.io/';
+}
 
 var nw = new NwBuilder({
-    files: ['./src/**', './node_modules/**', './package.json','./install','LICENSE.txt','CHANGELOG.md','README.md'],
+    files: ['./src/**', './node_modules/**', './package.json', './install', 'LICENSE.txt', 'CHANGELOG.md', 'README.md'],
     version: nwVersion,
     zip: false,
-    downloadUrl: 'https://get.popcorntime.sh/repo/nw/',
-    platforms: argv.p ? argv.p.split(',') : [detectCurrentPlatform()]
+    downloadUrl: buildDownloadUrl,
+    platforms: buildplatforms,
 }).on('log', console.log);
 
 gulp.task('run', function() {
@@ -38,28 +50,28 @@ gulp.task('clean', function() {
     return del('build/');
 });
 
-gulp.task('zip', function () {
+gulp.task('zip', function() {
 
     gulp.src('./build/Popcorn-Time-CE/osx64/**/*')
-        .pipe(tar('popcorn-time-ce_osx64_'+package.version+'.tar'))
+        .pipe(tar('popcorn-time-ce_osx64_' + package.version + '.tar'))
         .pipe(gzip())
         .pipe(gulp.dest('./dist'));
     gulp.src('./build/Popcorn-Time-CE/win32/**')
-        .pipe(zip('popcorn-time-ce_win32_'+package.version+'.zip'))
+        .pipe(zip('popcorn-time-ce_win32_' + package.version + '.zip'))
         .pipe(gulp.dest('./dist'));
     gulp.src('./build/Popcorn-Time-CE/win64/**')
-        .pipe(zip('popcorn-time-ce_win64_'+package.version+'.zip'))
+        .pipe(zip('popcorn-time-ce_win64_' + package.version + '.zip'))
         .pipe(gulp.dest('./dist'));
     gulp.src('./build/Popcorn-Time-CE/linux32/**')
-        .pipe(tar('popcorn-time-ce_linux32_'+package.version+'.tar'))
+        .pipe(tar('popcorn-time-ce_linux32_' + package.version + '.tar'))
         .pipe(gzip())
         .pipe(gulp.dest('./dist'));
     return gulp.src('./build/Popcorn-Time-CE/linux64/**')
-        .pipe(tar('popcorn-time-ce_linux64_'+package.version+'.tar'))
+        .pipe(tar('popcorn-time-ce_linux64_' + package.version + '.tar'))
         .pipe(gzip())
         .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('default', function() {
-  // place code for your default task here
+    // place code for your default task here
 });
